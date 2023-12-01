c8b22ee0de70205da4194112226c8041cfaf7cce
“Kateryna <kathreen14@gmail.com>
2023-12-01 15:17:54 +0200

    added file HW3

--- folder_with_files/HW3.js
+++ folder_with_files/HW3.js
@@ -5,5 +5,7 @@ function gasStation(carFuel, carSize) {
   console.log(`Заправка для ${carSize} авто, тип палива ${carFuel}`)
 }
 
-gasStation('diesel', 'XL')
+let carFuel = 'gas';
+let carSize = 'XL';
+gasStation(carFuel, carSize)
 
