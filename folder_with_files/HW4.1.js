09ae658edbfb33ea2b2c44d872ffafa5f7d8b2b6
“Kateryna <kathreen14@gmail.com>
2023-12-05 17:47:56 +0200

    added file HW4.1

---
+++ folder_with_files/HW4.1.js
@@ -0,0 +1,31 @@
+const { reverse } = require("dns");
+
+// Reverce 
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+console.log(reverseString("apple"));
+
+//Palindrome
+function palindrome(str) {
+  const cleanStr = str.replace(/\s/g, '').toLowerCase();
+  return cleanStr === cleanStr.split('').reverse().join('');
+}
+console.log(palindrome("level"))
+console.log(palindrome("Orange"))
+
+//Extract even elements from arr --> reverce it 
+
+function even(a) {
+    let ar = [];
+
+     for (let i = 0; i < a.length; i++) {
+        if (a[i] % 2 === 0) 
+        ar.push(a[i]);
+     }
+    return ar.reverse();
+}
+
+let arr = [2, 5, 9, 12, 25, 30, 51, 66];
+
+console.log (even(arr));
